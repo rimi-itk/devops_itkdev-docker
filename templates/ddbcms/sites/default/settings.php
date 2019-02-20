866da2851e59e4d964da69e3e9d26fd973551c83
Jesper Kristensen <cableman@linuxdev.dk>
2019-02-20 12:26:04 +0100

    Added memcached to ddb template

--- templates/ddbcms/sites/default/settings.php
+++ templates/ddbcms/sites/default/settings.php
@@ -27,3 +27,20 @@ $conf['404_fast_paths_exclude'] = '/\/(?:styles)|(?:system\/files)\//';
 $conf['404_fast_paths'] = '/\.(?:txt|png|gif|jpe?g|css|js|ico|swf|flv|cgi|bat|pl|dll|exe|asp)$/i';
 $conf['404_fast_html'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head><title>404 Not Found</title></head><body><h1>Not Found</h1><p>The requested URL "@path" was not found on this server.</p></body></html>';
 
+/**
+ * Memcached configuration.
+ */
+include_once('./includes/cache.inc');
+include_once('./profiles/ding2/modules/contrib/memcache/memcache.inc');
+// Forms cache table.
+$conf['cache_class_cache_form'] = 'DrupalDatabaseCache';
+$conf['cache_backends'][] = 'profiles/ding2/modules/contrib/memcache/memcache.inc';
+$conf['cache_default_class'] = 'MemCacheDrupal';
+$conf['memcache_key_prefix'] = $databases['default']['default']['database'];
+$conf['memcache_servers'] = array(
+  'memcached:11211' => 'default',
+);
+$conf['memcache_bins'] = array(
+  'cache' => 'default',
+);
+$conf['memcache_log_data_pieces'] = 10;
