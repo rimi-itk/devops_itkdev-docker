d9d74f16863570218e50f82f6c4b91ade0e7ddbb
Jesper Kristensen <cableman@linuxdev.dk>
2019-06-21 11:52:20 +0200

    Added perl fcgi

---
+++ perl5-fcgi/docker-entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e  
+
+## Run templates with configuration.
+/usr/local/bin/confd --onetime --backend env --confdir /etc/confd
+
+## Start the perl fcgi process.
+/usr/sbin/fcgiwrap -s tcp:0.0.0.0:9000 -f
