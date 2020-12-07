b0bda4abc3e81486c348ea85170e6d39279fdc78
Jesper Kristensen <cableman@linuxdev.dk>
2020-12-07 21:51:03 +0100

    Added php8.0 image

---
+++ php8.0-fpm/docker-entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+# Add docker host domain to hosts file
+if [ ! -z "${DOCKER_HOST_DOMAIN}" ]; then
+  echo $(getent hosts host.docker.internal | cut -d" " -f1) ${DOCKER_HOST_DOMAIN} >> /etc/hosts
+fi
+
+# Set xdebug remote host to docker host ip.
+if [ ! -z "${PHP_XDEBUG}" ]; then
+  export PHP_XDEBUG_REMOTE_HOST=$(getent hosts host.docker.internal | cut -d" " -f1)
+fi
+
+## Run templates with configuration.
+/usr/local/bin/confd --onetime --backend env --confdir /etc/confd
+
+## Set selected composer version. Default version 1.
+if [ ! -z "${COMPOSER_VERSION}" ]; then
+  if [ "${COMPOSER_VERSION}" = "1" ]; then
+    ln -fs /usr/local/bin/composer1 /usr/local/bin/composer
+  else
+    ln -fs /usr/local/bin/composer2 /usr/local/bin/composer
+  fi
+else
+  ln -fs /usr/local/bin/composer1 /usr/local/bin/composer
+fi
+
+## Start the php FPM process.
+echo "Starting PHP 7.4 FPM"
+php-fpm7.4 -F --pid /var/run/php/php-fpm7.4.pid -y /etc/php/7.4/fpm/php-fpm.conf
