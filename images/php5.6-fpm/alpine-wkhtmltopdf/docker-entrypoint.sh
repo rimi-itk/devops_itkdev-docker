5afc640e8fde0343c82ebaf215be6cd5183fd26c
Jesper Kristensen <cableman@linuxdev.dk>
2022-03-25 14:49:28 +0100

    SUPP0RT-445: Added php5.6 wkhtmlpdf image

---
+++ images/php5.6-fpm/alpine-wkhtmltopdf/docker-entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -e
+
+# Add docker host domain to hosts file
+if [ ! -z "${DOCKER_HOST_DOMAIN}" ]; then
+  echo $(getent hosts host.docker.internal | cut -d" " -f1) ${DOCKER_HOST_DOMAIN} >> /etc/hosts
+fi
+
+## Set selected composer version. Default version 1.
+if [ ! -z "${COMPOSER_VERSION}" ]; then
+  if [ "${COMPOSER_VERSION}" = "1" ]; then
+    ln -fs /usr/bin/composer1 /usr/local/bin/composer
+  else
+    ln -fs /usr/bin/composer2 /usr/local/bin/composer
+  fi
+else
+  ln -fs /usr/bin/composer1 /usr/local/bin/composer
+fi
+
+## Start the php FPM process.
+echo "Starting PHP 5.6 FPM"
+/usr/local/bin/docker-php-entrypoint php-fpm
